fc7597c88b2c89fcdd02c271cc749cce60cda9ac
Shen Wang <shengqiw97@gmail.com>
2025-02-13 20:08:28 -0600

    feat: add title and basic mui

---
+++ web-app/src/components/page-layout.tsx
@@ -0,0 +1,11 @@
+import { ReactNode } from "react";
+
+export const PageLayout = ({ children }: {children: ReactNode}) => {
+
+
+    return (
+        <>
+            {children}
+        </>
+    );
+}

fa5520b20524f4ff7fbe5b2faf9e700cda46b7cd
Shen Wang <shengqiw97@gmail.com>
2025-02-14 22:13:09 -0600

    fix: remove boilerplate nextjs

--- web-app/src/app/layout.tsx
+++ web-app/src/app/layout.tsx
@@ -1,17 +1,7 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { PageLayout } from "@/components/page-layout";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import { ReactChild } from "@/types/generic-props";
 
 export const metadata: Metadata = {
   title: "Cozy Threads",
@@ -20,12 +10,10 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+}: Readonly<ReactChild>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body>
       <PageLayout>
         {children}
       </PageLayout>

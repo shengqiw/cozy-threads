8bf187649dde4cca3bfb2a0c0e878d69b3177696
Shen Wang <shengqiw97@gmail.com>
2025-02-15 12:15:54 -0600

    feat: add payment route

---
+++ web-app/src/app/api/checkout/route.ts
@@ -0,0 +1,42 @@
+import { NextResponse } from "next/server";
+import { headers } from "next/headers";
+
+import { stripe } from "@/lib/stripe";
+import { CartItem } from "@/types/cart";
+
+// Visa	4242424242424242	Any 3 digits	Any future date
+
+export async function POST(request: Request) {
+  const body = await request.json();
+
+  if (!body.cartItems || !Object.keys(body.cartItems).length)
+    throw new Error("No items in cart");
+
+  try {
+    const headersList = await headers();
+    const origin = headersList.get("origin");
+    if (!stripe)
+      throw new Error("Stripe is not configured properly. Check missing key");
+    // Create Checkout Sessions from body params.
+    const session = await stripe.checkout.sessions.create({
+      line_items: (Object.values(body.cartItems) as CartItem[]).map(
+        (item: CartItem) => ({
+          price: item.id,
+          quantity: item.count,
+        })
+      ),
+      mode: "payment",
+      success_url: `${origin}/payment/success?session_id={CHECKOUT_SESSION_ID}`,
+      cancel_url: `${origin}/payment?canceled=true`,
+    });
+
+    if (!session || !session.url) throw new Error("Failed to create session");
+    return Response.json(session);
+  } catch (err: unknown) {
+    if (err instanceof Error) {
+      return NextResponse.json({ error: err.message }, { status: 500 });
+    } else {
+      return NextResponse.json({ error: "Unknown error" }, { status: 500 });
+    }
+  }
+}
